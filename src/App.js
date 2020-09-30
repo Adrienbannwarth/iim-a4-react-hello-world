168d24db462c0a706554c01a8dd4eb00143ee413
adrienbannwarth <adrien.bann@gmail.com>
2020-09-30 12:20:46 +0200

    add travis file

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         WESH DENE
+         WESH DENE  
         </p>
+        <p>16</p>
         <a
           className="App-link"
           href="https://reactjs.org"
